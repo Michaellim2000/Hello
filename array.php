dbeb1d27e93b160db82899b0d7324adf22d6d33c
Michaellim2000 <michael18001@mail.unpad.ac.id>
2020-07-30 19:26:39 +0700

    Testingan Hello

--- array.php
+++ array.php
@@ -1,16 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title> Array </title>
-</head>
-<body>
-    <h1>Berlatih Array</h1>
-        
-    <?php 
-        echo "<h3> Soal 1 </h3>";
+ "<h3> Soal 1 </h3>";
         /* 
             SOAL NO 1
             Kelompokkan nama-nama di bawah ini ke dalam Array.
